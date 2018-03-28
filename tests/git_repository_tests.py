e74e3ed37c10153491d12aa372c2b209fc274597
ishepard <spadini.davide@gmail.com>
2018-03-28 11:41:30 +0200

    better handling of dates in the test

--- tests/git_repository_tests.py
+++ tests/git_repository_tests.py
@@ -20,18 +20,17 @@ class GitRepositoryTests(unittest.TestCase):
         gr = GitRepository('../test-repos/test1/')
         self.assertIsNotNone(gr)
         change_sets = gr.get_change_sets()
-        tozone = tz.gettz('GMT+1')
-
+        to_zone = tz.gettz('GMT+1')
 
         cs1 = ChangeSet('a88c84ddf42066611e76e6cb690144e5357d132c',
-                        datetime.fromtimestamp(1521711671).replace(tzinfo=tozone))
+                        datetime(2018, 3, 22, 10, 41, 11, tzinfo=to_zone))
         cs2 = ChangeSet('6411e3096dd2070438a17b225f44475136e54e3a',
-                        datetime.fromtimestamp(1521711707).replace(tzinfo=tozone))
+                        datetime(2018, 3, 22, 10, 41, 47, tzinfo=to_zone))
         cs3 = ChangeSet('09f6182cef737db02a085e1d018963c7a29bde5a',
-                        datetime.fromtimestamp(1521711723).replace(tzinfo=tozone))
-        tozone = tz.gettz('GMT+2')
+                        datetime(2018, 3, 22, 10, 42, 3, tzinfo=to_zone))
+        to_zone = tz.gettz('GMT+2')
         cs4 = ChangeSet('1f99848edadfffa903b8ba1286a935f1b92b2845',
-                        datetime.fromtimestamp(1522163452).replace(tzinfo=tozone))
+                        datetime(2018, 3, 27, 17, 10, 52, tzinfo=to_zone))
 
         self.assertIn(cs1, change_sets)
         self.assertIn(cs2, change_sets)
@@ -42,21 +41,26 @@ class GitRepositoryTests(unittest.TestCase):
     def test_get_commit(self):
         gr = GitRepository('../test-repos/test1/')
         c = gr.get_commit('09f6182cef737db02a085e1d018963c7a29bde5a')
-        tozone = tz.gettz('GMT+1')
+        to_zone = tz.gettz('GMT+1')
 
-        print(c)
         self.assertEqual('09f6182cef737db02a085e1d018963c7a29bde5a', c.hash)
         self.assertEqual('ishepard', c.author.name)
         self.assertEqual('ishepard', c.committer.name)
-        self.assertEqual(datetime.fromtimestamp(1521711723).replace(tzinfo=tozone).timestamp(), c.date.timestamp())
+        self.assertEqual(datetime(2018,3,22,10,42,3,tzinfo=to_zone).timestamp(), c.date.timestamp())
         self.assertEqual(1, len(c.modifications))
         self.assertEqual('Ooops file2', c.msg)
 
     def test_get_first_commit(self):
         gr = GitRepository('../test-repos/test1/')
         c = gr.get_commit('a88c84ddf42066611e76e6cb690144e5357d132c')
-        print(c)
-        tozone = tz.gettz('GMT+1')
+        to_zone = tz.gettz('GMT+1')
+
+        self.assertEqual('a88c84ddf42066611e76e6cb690144e5357d132c', c.hash)
+        self.assertEqual('ishepard', c.author.name)
+        self.assertEqual('ishepard', c.committer.name)
+        self.assertEqual(datetime(2018,3,22,10,41,11,tzinfo=to_zone).timestamp(), c.date.timestamp())
+        self.assertEqual(2, len(c.modifications))
+        self.assertEqual('First commit adding 2 files', c.msg)
 
     def test_checkout(self):
         gr = GitRepository('../test-repos/test1/')
@@ -78,7 +82,6 @@ class GitRepositoryTests(unittest.TestCase):
 
     def test_total_commits(self):
         gr = GitRepository('../test-repos/test1/')
-
         self.assertEqual(5, gr.total_commits())
 
     def test_get_commit_from_tag(self):

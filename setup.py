370ec70c699602ead0b9364e0ad05992a0163970
ishepard <spadini.davide@gmail.com>
2018-04-07 12:45:54 +0200

    update setup.py with long description

--- setup.py
+++ setup.py
@@ -3,18 +3,41 @@ from setuptools import setup, find_packages
 install_requires = ['gitpython']
 test_requires = ['pytest', 'python-dateutil', 'psutil']
 
+# Get the long description from the relevant file
+long_description = 'PyDriller is a Python framework that helps developers on mining software repositories. ' \
+                   'Born as a fork of RepoDriller, with PyDriller' \
+                   ' you can easily extract information from any Git repository, such as commits, developers, ' \
+                   'modifications, diffs, and source codes, and quickly export CSV files.'
 
 setup(
     name='pydriller',
     description='Framework for MSR',
+    long_description = long_description,
     author='Davide Spadini',
     author_email='spadini.davide@gmail.com',
-    version='1.0',
+    version='1.0.1',
     packages=find_packages('.'),
     url='https://github.com/ishepard/pydriller',
     license='Apache License',
     package_dir={'pydriller': 'pydriller'},
     python_requires='>=3.6',
     install_requires=install_requires,
-    test_requirements=test_requires + install_requires
+    test_requirements=test_requires + install_requires,
+    classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            "Operating System :: OS Independent",
+            "Operating System :: POSIX",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: MacOS :: MacOS X",
+            ]
 )

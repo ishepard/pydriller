2e38efc06487d7ea8a3fba8cedbbbd1d2d9cf55b
ishepard <spadini.davide@gmail.com>
2018-04-07 10:00:18 +0200

    small in type hinting

--- pydriller/git_repository.py
+++ pydriller/git_repository.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from pprint import pprint
-from typing import List, Dict
+from typing import List, Dict, Tuple
 from git import Git, Repo, Diff, GitCommandError
 from git.objects.util import tzoffset
 from pydriller.domain.commit import Commit, ChangeSet
@@ -218,7 +218,7 @@ class GitRepository:
             logging.debug('Tag {} not found'.format(tag))
             raise
 
-    def parse_diff(self, diff: str) -> Dict[str, List[(int, str)]]:
+    def parse_diff(self, diff: str) -> Dict[str, List[Tuple(int, str)]]:
         """
         Given a diff, returns a dictionary with the added and deleted lines.
         The dictionary has 2 keys: "added" and "deleted", each containing the

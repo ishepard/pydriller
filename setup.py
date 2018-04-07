7767632327433ef0ad96a7231fce68808d4ffe74
ishepard <spadini.davide@gmail.com>
2018-04-07 11:20:07 +0200

    minor in setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ test_requires = ['pytest', 'python-dateutil', 'psutil']
 
 
 setup(
-    author_email='spadini.davide@gmail.com',
-    description='Framework for MSR',
     name='pydriller',
-    version='1.0.2',
+    description='Framework for MSR',
+    author='Davide Spadini',
+    author_email='spadini.davide@gmail.com',
+    version='1.0',
     packages=find_packages('.'),
     url='https://github.com/ishepard/pydriller',
+    license='Apache License',
     package_dir={'pydriller': 'pydriller'},
-    license='BSD License',
-    author='Davide Spadini',
     python_requires='>=3.6',
     install_requires=install_requires,
     test_requirements=test_requires + install_requires

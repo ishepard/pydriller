14fa582c259b8050560bc6aedb8acbb223609e67
ishepard <spadini.davide@gmail.com>
2018-06-09 13:09:12 +0200

    preparing for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description = long_description,
     author='Davide Spadini',
     author_email='spadini.davide@gmail.com',
-    version='1.2',
+    version='1.2.1',
     packages=find_packages('.'),
     url='https://github.com/ishepard/pydriller',
     license='Apache License',

4433dec888fbe4581bd2c0889b5945c041a19e0f
ishepard <spadini.davide@gmail.com>
2018-11-02 10:02:07 +0100

    preparing for release.

--- pydriller/domain/commit.py
+++ pydriller/domain/commit.py
@@ -404,23 +404,17 @@ class Commit:
             return self.__dict__ == other.__dict__
 
     def __str__(self):
-        return ('Hash: {}'.format(self.hash) + '\n'
-                                               'Author: {}'.format(self.author.name) + '\n'
-                                                                                       'Author email: {}'.format(
-            self.author.email) + '\n'
-                                 'Committer: {}'.format(self.committer.name) + '\n'
-                                                                               'Committer email: {}'.format(
-            self.committer.email) + '\n'
-                                    'Author date: {}'.format(self.author_date.strftime("%Y-%m-%d %H:%M:%S")) + '\n'
-                                                                                                               'Committer date: {}'.format(
-            self.committer_date.strftime("%Y-%m-%d %H:%M:%S")) + '\n'
-                                                                 'Message: {}'.format(self.msg) + '\n'
-                                                                                                  'Parent: {}'.format(
-            "\n".join(map(str, self.parents))) + '\n'
-                                                 'Merge: {}'.format(self.merge) + '\n'
-                                                                                  'Modifications: \n{}'.format(
-            "\n".join(map(str, self.modifications))) + '\n'
-                                                       'Branches: \n{}'.format(
-            "\n".join(map(str, self.branches))) + '\n'
-                                                  'In main branch: {}'.format(self.in_main_branch)
+        return ('Hash: {}\n'.format(self.hash) +
+                'Author: {}\n'.format(self.author.name) +
+                'Author email: {}\n'.format(self.author.email) +
+                'Committer: {}\n'.format(self.committer.name) +
+                'Committer email: {}\n'.format(self.committer.email) +
+                'Author date: {}\n'.format(self.author_date.strftime("%Y-%m-%d %H:%M:%S")) +
+                'Committer date: {}\n'.format(self.committer_date.strftime("%Y-%m-%d %H:%M:%S")) +
+                'Message: {}\n'.format(self.msg) +
+                'Parent: {}\n'.format("\n".join(map(str, self.parents))) +
+                'Merge: {}\n'.format(self.merge) +
+                'Modifications: \n{}'.format("\n".join(map(str, self.modifications))) +
+                'Branches: \n{}'.format("\n".join(map(str, self.branches))) +
+                'In main branch: {}\n'.format(self.in_main_branch)
                 )

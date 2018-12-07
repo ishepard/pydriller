aee50f80543ec27bc5d4d490620602bc87fa6794
ishepard <spadini.davide@gmail.com>
2018-12-07 16:23:00 +0100

    [skip travis] small in example

--- examples/only_commits_that_modified_java_files.py
+++ examples/only_commits_that_modified_java_files.py
@@ -1,4 +1,4 @@
 from pydriller import RepositoryMining
 
 for commit in RepositoryMining('../test-repos/test1/', only_modifications_with_file_types=['.java']).traverse_commits():
-    print('Hash {} modified a java file'.format(commit.hash, ', '.join(mod.filename for mod in commit.modifications)))
+    print('Hash {} modified a java file'.format(commit.hash))

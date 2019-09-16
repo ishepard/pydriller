fbd96ac6c02363fc91e456d9442dd09cdf6cc8c3
ishepard <spadini.davide@gmail.com>
2019-09-16 14:45:57 +0200

    change logging level in test_diff_performances

--- tests/test_memory_consumption.py
+++ tests/test_memory_consumption.py
@@ -20,14 +20,62 @@ import psutil
 
 import requests
 import logging
+from git import Repo
+from pydriller.repository_mining import RepositoryMining
+from datetime import datetime, timezone
 
 if 'MEM_CONS_TEST' in os.environ:
     logging.basicConfig(level=logging.WARNING)
     webhook_url = os.environ['WEBHOOK_URL']
 
-from git import Repo
-from pydriller.repository_mining import RepositoryMining
-from datetime import datetime, timezone
+
+def test_performances_diff(caplog):
+    if not 'MEM_CONS_TEST' in os.environ:
+        return
+    caplog.set_level(logging.WARNING)
+    gitpythonrepo = Repo('test-repos/hadoop')
+    start = datetime(2017, 1, 1, tzinfo=timezone.utc)
+    end = datetime(2018, 1, 1, tzinfo=timezone.utc)
+
+    dt1 = datetime.now()
+    for commit in RepositoryMining('test-repos/hadoop',
+                                   since=start, to=end).traverse_commits():
+        if len(commit.parents) == 1:
+            for mod in commit.modifications:
+                mod.source_code
+                mod.source_code_before
+                mod.diff
+    dt2 = datetime.now()
+    logging.warning('pydriller: it took {}'.format(dt2 - dt1))
+
+    dt1 = datetime.now()
+    for commit in gitpythonrepo.iter_commits():
+        if commit.committed_datetime < start or commit.committed_datetime > \
+                end:
+            continue
+        if len(commit.parents) == 1:
+            diff_index = commit.parents[0].diff(commit, create_patch=True)
+            for diff in diff_index:
+                diff.a_path
+                diff.b_path
+                diff.diff.decode('utf-8', 'ignore')
+                try:
+                    if diff.deleted_file:
+                        diff.a_blob.data_stream.read().decode('utf-8',
+                                                              'ignore')
+                    elif diff.new_file:
+                        diff.b_blob.data_stream.read().decode('utf-8',
+                                                              'ignore')
+                    else:
+                        diff.a_blob.data_stream.read().decode('utf-8',
+                                                              'ignore')
+                        diff.b_blob.data_stream.read().decode('utf-8',
+                                                              'ignore')
+                except (UnicodeDecodeError, AttributeError, ValueError):
+                    pass
+    dt2 = datetime.now()
+    logging.warning('gitpython: it took {}'.format(dt2 - dt1))
+
 
 def test_memory(caplog):
     if not 'MEM_CONS_TEST' in os.environ:
@@ -134,50 +182,3 @@ def mine(_type):
     diff = end - start
 
     return diff, all_commits
-
-
-def test_performances_diff():
-    if not 'MEM_CONS_TEST' in os.environ:
-        return
-    gitpythonrepo = Repo('test-repos/hadoop')
-    start = datetime(2017, 1, 1, tzinfo=timezone.utc)
-    end = datetime(2018, 1, 1, tzinfo=timezone.utc)
-
-    dt1 = datetime.now()
-    for commit in RepositoryMining('test-repos/hadoop',
-                                   since=start, to=end).traverse_commits():
-        if len(commit.parents) == 1:
-            for mod in commit.modifications:
-                mod.source_code
-                mod.source_code_before
-                mod.diff
-    dt2 = datetime.now()
-    logging.warning('pydriller: it took {}'.format(dt2 - dt1))
-
-    dt1 = datetime.now()
-    for commit in gitpythonrepo.iter_commits():
-        if commit.committed_datetime < start or commit.committed_datetime > \
-                end:
-            continue
-        if len(commit.parents) == 1:
-            diff_index = commit.parents[0].diff(commit, create_patch=True)
-            for diff in diff_index:
-                diff.a_path
-                diff.b_path
-                diff.diff.decode('utf-8', 'ignore')
-                try:
-                    if diff.deleted_file:
-                        diff.a_blob.data_stream.read().decode('utf-8',
-                                                              'ignore')
-                    elif diff.new_file:
-                        diff.b_blob.data_stream.read().decode('utf-8',
-                                                              'ignore')
-                    else:
-                        diff.a_blob.data_stream.read().decode('utf-8',
-                                                              'ignore')
-                        diff.b_blob.data_stream.read().decode('utf-8',
-                                                              'ignore')
-                except (UnicodeDecodeError, AttributeError, ValueError):
-                    pass
-    dt2 = datetime.now()
-    logging.warning('gitpython: it took {}'.format(dt2 - dt1))

97dd5e9466d472e35fa4872a670eace3c5114a6c
ishepard <spadini.davide@gmail.com>
2019-12-17 13:50:50 +0100

    Prepare for release

--- setup.py
+++ setup.py
@@ -16,18 +16,20 @@ test_requires = read_reqs("test-requirements.txt")
 
 
 # Get the long description from the relevant file
-long_description = 'PyDriller is a Python framework that helps developers on mining software repositories. ' \
-                   'With PyDriller' \
-                   ' you can easily extract information from any Git repository, such as commits, developers, ' \
-                   'modifications, diffs, and source codes, and quickly export CSV files.'
+long_description = 'PyDriller is a Python framework that helps developers on ' \
+                   'mining software repositories. With PyDriller' \
+                   ' you can easily extract information from any Git ' \
+                   'repository, such as commits, developers, ' \
+                   'modifications, diffs, and source codes, and ' \
+                   'quickly export CSV files.'
 
 setup(
     name='PyDriller',
     description='Framework for MSR',
-    long_description = long_description,
+    long_description=long_description,
     author='Davide Spadini',
     author_email='spadini.davide@gmail.com',
-    version='1.9.2',
+    version='1.10',
     packages=find_packages('.'),
     url='https://github.com/ishepard/pydriller',
     license='Apache License',
@@ -40,15 +42,15 @@ setup(
             #   3 - Alpha
             #   4 - Beta
             #   5 - Production/Stable
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Environment :: Console',
             'Intended Audience :: Developers',
             'Intended Audience :: Science/Research',
             'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
             'Topic :: Software Development :: Libraries :: Python Modules',
             "Operating System :: OS Independent",
             "Operating System :: POSIX",

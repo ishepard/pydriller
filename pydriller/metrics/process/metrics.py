0b5a0228d28bcc45276df52233aa020a0e419458
stefanodallapalma <stefano.dallapalma0@gmail.com>
2020-01-17 16:01:30 +0100

    Added metric to measure experience of the highest contributor of a file

--- pydriller/metrics/process/metrics.py
+++ pydriller/metrics/process/metrics.py
@@ -11,8 +11,11 @@ at a given commit:
       of added lines) added lines in the file
     * Normalized Lines Deleted: are the normalized (by the total number \
       of deleted lines) deleted lines in the file
-    * Owner's Contributed Lines: measures the percentage of the lines \
-      authored by the highest contributor of a file
+    * Owner's Contributed Lines: is the percentage of the lines authored by \
+      the highest contributor of a file.
+    * Owner's Experience: is the experience of the highest contributor of a file \
+      using the percent of lines he authored in the project at a given point \
+      in time.
     * Minor Contributor Count: measures the number of contributors who \
       authored less than 5% of the code of a file
 
@@ -23,7 +26,7 @@ See https://ieeexplore.ieee.org/document/6606589 for more info.
 from pydriller.metrics.process.commit_count import CommitCount
 from pydriller.metrics.process.devs_count import DevsCount
 from pydriller.metrics.process.lines_count import NormalizedLinesCount
-from pydriller.metrics.process.owner_lines_count import OwnersContributedLines
+from pydriller.metrics.process.owner_experience import OwnerExperience
 from pydriller.metrics.process.minor_contributor_count import MinorContributorCount
 
 def commits_count(path_to_repo: str, filepath: str, to_commit: str = None):
@@ -36,8 +39,8 @@ def norm_lines_count(path_to_repo: str, filepath: str, to_commit: str = None):
     return NormalizedLinesCount(path_to_repo, filepath,
                                 to_commit=to_commit).count()
 
-def owner_contributed_lines(path_to_repo: str, filepath: str, to_commit: str = None):
-    return OwnersContributedLines(path_to_repo, filepath,
+def owner_experience(path_to_repo: str, filepath: str, to_commit: str = None):
+    return OwnerExperience(path_to_repo, filepath,
                                 to_commit=to_commit).count()
 
 def minor_contributors_count(path_to_repo: str, filepath: str, to_commit: str = None):
--- tests/metrics/process/test_owner_experience.py
+++ tests/metrics/process/test_owner_experience.py
@@ -1,14 +1,14 @@
 import pytest
-from pydriller.metrics.process.owner_lines_count import OwnersContributedLines
+from pydriller.metrics.process.owner_experience import OwnerExperience
 
 test_data = [
-   ('https://github.com/ishepard/pydriller', 'unexisting.java', None, '115953109b57d841ccd0952d70f8ed6703d175cd', 0.0),
-   ('https://github.com/ishepard/pydriller', 'pydriller/repository_mining.py', None, '5a1a2b89c53a115e8087408469da04a7156ad808', float(12/21))
+   ('https://github.com/ishepard/pydriller', 'unexisting.java', None, '115953109b57d841ccd0952d70f8ed6703d175cd', (0, 0)),
+   ('https://github.com/ishepard/pydriller', 'pydriller/repository_mining.py', None, '5a1a2b89c53a115e8087408469da04a7156ad808', (round(12/21, 4), round(23/10835, 4)))
 ]
 
 @pytest.mark.parametrize('path_to_repo, filepath, from_commit, to_commit, expected', test_data)
 def test(path_to_repo, filepath, from_commit, to_commit, expected):
-    metric = OwnersContributedLines(path_to_repo=path_to_repo,
+    metric = OwnerExperience(path_to_repo=path_to_repo,
                                     filepath=filepath,
                                     from_commit=from_commit,
                                     to_commit=to_commit)

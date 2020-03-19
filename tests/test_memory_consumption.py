00c70d19f28db0b099febde2ecbf79a7cca735ad
ishepard <spadini.davide@gmail.com>
2020-03-19 11:00:52 +0100

    removing memory consumption test. it introduces too much flakiness, I will run it locally

--- tests/test_memory_consumption.py
+++ tests/test_memory_consumption.py
@@ -12,26 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
+import logging
 import os
 import platform
 import sys
-
 import psutil
-
-if 'MEM_CONS_TEST' in os.environ:
-    import requests
-    import logging
-
-    logging.basicConfig(level=logging.WARNING)
-    webhook_url = os.environ['WEBHOOK_URL']
-
-from pydriller.repository_mining import RepositoryMining
+from pydriller import RepositoryMining
 from datetime import datetime
+
+logging.basicConfig(level=logging.WARNING)
+skip_remote = True
 
 
 def test_memory(caplog):
-    if 'MEM_CONS_TEST' not in os.environ:
+    if skip_remote:
         return
     caplog.set_level(logging.WARNING)
 
@@ -74,17 +68,17 @@ def test_memory(caplog):
             minutes_with_metrics >= 7:
         # if to analyze 1000 commits requires more than 250MB of RAM,
         # more than 1 minute without metrics or
-        # 7 minutes with metrics, print it on the Slack channel
-        logs_and_post_on_slack(diff_with_nothing, all_commits_with_nothing,
-                               diff_with_everything, all_commits_with_everything,
-                               diff_with_metrics, all_commits_with_metrics)
+        # 7 minutes with metrics, print it
+        log(diff_with_nothing, all_commits_with_nothing,
+            diff_with_everything, all_commits_with_everything,
+            diff_with_metrics, all_commits_with_metrics)
 
     assert 973 == len(all_commits_with_nothing) == len(all_commits_with_everything) == len(all_commits_with_metrics)
 
 
-def logs_and_post_on_slack(diff_with_nothing, all_commits_with_nothing,
-                           diff_with_everything, all_commits_with_everything,
-                           diff_with_metrics, all_commits_with_metrics):
+def log(diff_with_nothing, all_commits_with_nothing,
+        diff_with_everything, all_commits_with_everything,
+        diff_with_metrics, all_commits_with_metrics):
     text = "*PYTHON V{}.{} - System: {}*\n" \
            "*Max memory (MB)*\n" \
            "With nothing: {}, with everything: {}, with metrics: {}\n" \
@@ -96,24 +90,19 @@ def logs_and_post_on_slack(diff_with_nothing, all_commits_with_nothing,
            "*Commits per second:*\n" \
            "With nothing: {}, with everything: {}, with metrics: {}"
 
-    slack_data = {
-        'text': text.format(
-            sys.version_info[0], sys.version_info[1], platform.system(),
-            max(all_commits_with_nothing), max(all_commits_with_everything), max(all_commits_with_metrics),
-            min(all_commits_with_nothing), min(all_commits_with_everything), min(all_commits_with_metrics),
-            diff_with_nothing.seconds // 3600, (diff_with_nothing.seconds % 3600) // 60, diff_with_nothing.seconds % 60,
-            diff_with_everything.seconds // 3600, (diff_with_everything.seconds % 3600) // 60,
-            diff_with_everything.seconds % 60,
-            diff_with_metrics.seconds // 3600, (diff_with_metrics.seconds % 3600) // 60, diff_with_metrics.seconds % 60,
-            len(all_commits_with_nothing),
-            len(all_commits_with_nothing) / diff_with_nothing.seconds,
-            len(all_commits_with_everything) / diff_with_everything.seconds,
-            len(all_commits_with_metrics) / diff_with_metrics.seconds
-        )}
-    requests.post(
-        webhook_url, data=json.dumps(slack_data),
-        headers={'Content-Type': 'application/json'}
-    )
+    print(text.format(
+        sys.version_info[0], sys.version_info[1], platform.system(),
+        max(all_commits_with_nothing), max(all_commits_with_everything), max(all_commits_with_metrics),
+        min(all_commits_with_nothing), min(all_commits_with_everything), min(all_commits_with_metrics),
+        diff_with_nothing.seconds // 3600, (diff_with_nothing.seconds % 3600) // 60, diff_with_nothing.seconds % 60,
+        diff_with_everything.seconds // 3600, (diff_with_everything.seconds % 3600) // 60,
+        diff_with_everything.seconds % 60,
+        diff_with_metrics.seconds // 3600, (diff_with_metrics.seconds % 3600) // 60, diff_with_metrics.seconds % 60,
+        len(all_commits_with_nothing),
+        len(all_commits_with_nothing) / diff_with_nothing.seconds,
+        len(all_commits_with_everything) / diff_with_everything.seconds,
+        len(all_commits_with_metrics) / diff_with_metrics.seconds
+    ))
 
 
 def mine(_type):
@@ -123,7 +112,7 @@ def mine(_type):
     all_commits = []
 
     start = datetime.now()
-    for commit in RepositoryMining('test-repos/hadoop',
+    for commit in RepositoryMining('test-repos-hadoop/hadoop',
                                    since=dt1,
                                    to=dt2).traverse_commits():
         memory = p.memory_info()[0] / (2 ** 20)

d5e2d346932a73eb9fa78cd1a966adb3100364f3
Davide Spadini <spadini.davide@gmail.com>
2020-10-08 12:39:32 +0200

    fix lgtm warning

--- setup.py
+++ setup.py
@@ -22,8 +22,7 @@ def get_version():
             if line.startswith('__version__'):
                 delim = '"' if '"' in line else "'"
                 return line.split(delim)[1]
-        else:
-            raise RuntimeError("Unable to find version string.")
+        raise RuntimeError("Unable to find version string.")
 
 
 setup(

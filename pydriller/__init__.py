2ee17e7dd6e24ce046d1eab6b3b1631d7a69cde9
Davide Spadini <spadini.davide@gmail.com>
2020-11-11 11:33:00 +0100

    preparing for release

--- pydriller/__init__.py
+++ pydriller/__init__.py
@@ -2,4 +2,4 @@
 from .domain.commit import Commit, Modification, ModificationType # noqa
 from .repository_mining import RepositoryMining, GitRepository    # noqa
 
-__version__ = "1.15.2"
+__version__ = "1.15.3"

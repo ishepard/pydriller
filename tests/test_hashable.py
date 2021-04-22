4a11d32805307b9a6d6553ab80dddda0d4d5c8b9
HelgeCPH <ropf@itu.dk>
2021-04-22 11:51:19 +0200

    Incorporated code review feedback

--- tests/test_hashable.py
+++ tests/test_hashable.py
@@ -29,6 +29,7 @@ def test_commit_dictset(repo: Git):
     assert commit_dict[c1] == "e7d13b0511f8a176284ce4f92ed8c6e8d09c77f2"
     assert commit_dict[c2] == c1.parents[0]
     assert commit_dict[c3] == "a4ece0762e797d2e2dcbd471115108dd6e05ff58"
+    assert commit_dict[c1] != commit_dict[c2]
 
     commit_set = {c1, c2, c3}
     assert type(commit_set) == set
@@ -50,50 +51,9 @@ def test_modification_dictset(repo: Git):
     assert mod_dict[m1].hash == "e7d13b0511f8a176284ce4f92ed8c6e8d09c77f2"
     assert mod_dict[m2s[0]].hash == c1.parents[0]
     assert mod_dict[m2s[1]].hash == c1.parents[0]
+    assert m1 != m2s[0]
 
     mod_set = {m1}.union(set(m2s))
     assert type(mod_set) == set
     assert m1 in mod_set
     assert mod_set - {m1} == set(m2s)
-
-
-# @pytest.mark.parametrize("repo", ["test-repos/complex_repo"], indirect=True)
-# def test_commit_dict(repo: Git):
-#     c1 = repo.get_commit("e7d13b0511f8a176284ce4f92ed8c6e8d09c77f2")
-#     c2 = repo.get_commit(c1.parents[0])
-#     c3 = repo.get_commit("a4ece0762e797d2e2dcbd471115108dd6e05ff58")
-
-#     commit_dict = {
-#         c1: c1.hash,
-#         c2: c2.hash,
-#         c3: c3.hash,
-#     }
-
-#     assert type(commit_dict) == dict
-#     assert commit_dict[c1] == "e7d13b0511f8a176284ce4f92ed8c6e8d09c77f2"
-#     assert commit_dict[c2] == c1.parents[0]
-#     assert commit_dict[c3] == "a4ece0762e797d2e2dcbd471115108dd6e05ff58"
-
-
-# def test_filename():
-#     diff_and_sc = {"diff": "", "source_code": "", "source_code_before": ""}
-#     m1 = Modification(
-#         "dspadini/pydriller/myfile.py",
-#         "dspadini/pydriller/mynewfile.py",
-#         ModificationType.ADD,
-#         diff_and_sc,
-#     )
-#     m3 = Modification(
-#         "dspadini/pydriller/myfile.py",
-#         "dspadini/pydriller/mynewfile.py",
-#         ModificationType.ADD,
-#         diff_and_sc,
-#     )
-#     m2 = Modification(
-#         "dspadini/pydriller/myfile.py", None, ModificationType.ADD, diff_and_sc
-#     )
-
-# assert m1.filename == "mynewfile.py"
-# assert m2.filename == "myfile.py"
-# assert m1 != m2
-# assert m3 == m1

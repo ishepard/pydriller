fd763873aa8e5dab074279b8397f12116d7b11d9
stefanodallapalma <stefano.dallapalmapalma0@gmail.com>
2022-06-14 12:24:39 +0200

    Fixed flake8 E302 and W293

--- tests/test_git_repository.py
+++ tests/test_git_repository.py
@@ -21,6 +21,7 @@ from git import Git as PyGit
 from pydriller.domain.commit import ModificationType
 from pydriller.git import Git
 
+
 @pytest.fixture
 def repo(request):
     gr = Git(request.param)
@@ -244,7 +245,7 @@ def test_should_detail_a_commit(repo: Git):
     assert commit.modified_files[0].new_path == "Matricula.java"
     assert commit.modified_files[0].diff.startswith("@@ -0,0 +1,62 @@\n+package model;") is True
     assert commit.modified_files[0].content.startswith("package model;") is True
-    
+
     with catch_warnings(record=True) as w:
         assert commit.modified_files[0].source_code.startswith("package model;") is True
         assert len(w) == 1

0ea6b69565855aa7861191b93ec19eb818d5c34f
ishepard <spadini.davide@gmail.com>
2025-03-26 11:38:25 +0100

    temporary disable git version check to fix doc build issue

--- pydriller/domain/__init__.py
+++ pydriller/domain/__init__.py
@@ -1,4 +1,4 @@
-from pydriller.utils.check_git_version import CheckGitVersion
-
-
-CheckGitVersion().check_git_version()
+# from pydriller.utils.check_git_version import CheckGitVersion
+#
+#
+# CheckGitVersion().check_git_version()
